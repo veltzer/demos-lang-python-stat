9fdb581dd29e971c8d5ecaa3b329d1a20c08cbf3
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 05:22:35 +0300

    fixing the build

---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "demos-statistics-python"
+project_long_description = "Demos for statistics in the python language"
+project_year_started = 2019
+project_description = "Demos for statistics in the python language"
---
+++ config/python.py
@@ -0,0 +1,24 @@
+import config.project
+
+package_name = config.project.project_name
+
+console_scripts = []
+setup_requires = []
+run_requires = []
+test_requires = []
+dev_requires = [
+    "pymakehelper",
+    "pydmt",
+    "pyclassifiers",
+    "numpy",
+    "matplotlib",
+    "scipy",
+]
+install_requires = list(setup_requires)
+install_requires.extend(run_requires)
+
+extras_require = {}
+
+python_requires = ">=3.9"
+test_os = ["ubuntu-20.04"]
+test_python = ["3.9"]
