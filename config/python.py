2331b0a184fc16ac8f87337e8f10f99b39725963
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-15 02:45:17 +0300

    better bootstrap

--- config/python.py
+++ config/python.py
@@ -1,11 +1,7 @@
-import config.project
-
-package_name = config.project.project_name
-
-dev_requires = [
+make_requires = [
     "pymakehelper",
-    "pydmt",
-    "pyclassifiers",
+]
+install_requires = [
     "numpy",
     "matplotlib",
     "scipy",
