daf20bf064e300c7117ca926d4adb06fc2a3c1e9
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:58 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "demos-statistics-python"
-project_long_description = "Demos for statistics in the python language"
-project_year_started = 2019
-project_description = "Demos for statistics in the python language"
+description_short = "Demos for statistics in the python language"
