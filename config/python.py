70b6f1a8a242b4d5c8a0bb6469cd1f1485682c67
Mark Veltzer <mark.veltzer@gmail.com>
2025-01-21 13:01:10 +0200

    no commit message given

--- config/python.py
+++ config/python.py
@@ -9,6 +9,11 @@ build_requires = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
+    "pycmdtools",
+    "pydmt",
+    "pylint",
+    "flake8",
+    "mypy",
 ]
 test_requires = []
 requires = config_requires + install_requires + build_requires + test_requires
--- src/pearson_spearman.py
+++ src/pearson_spearman.py
@@ -1,12 +1,14 @@
-# This example is taken from:
-# https://machinelearningmastery.com/how-to-use-correlation-to-understand-the-relationship-between-variables
+"""
+This example is taken from:
+https://machinelearningmastery.com/how-to-use-correlation-to-understand-the-relationship-between-variables
+"""
 
 from numpy import mean
 from numpy import std
 from numpy.random import randn
 from numpy.random import seed
-from matplotlib import pyplot
 from numpy import cov
+from matplotlib import pyplot
 from scipy.stats import pearsonr
 from scipy.stats import spearmanr
 
@@ -19,8 +21,8 @@ data2 = data1 + (10 * randn(1000) + 50)
 # data2 = 10 * randn(1000) + 50
 
 # summarize
-print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
-print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
+print(f"data1: mean={mean(data1)} stdv={std(data1)}")
+print(f"data2: mean={mean(data2)} stdv={std(data2)}")
 
 # plot
 pyplot.scatter(data1, data2)
@@ -34,9 +36,10 @@ print(covariance)
 # calculate Pearson's correlation
 # cov(X, Y) = (sum (x - mean(X)) * (y - mean(Y))) * 1/(n-1)
 corr, _ = pearsonr(data1, data2)
-print('Pearsons correlation: %.3f' % corr)
+print(f"Pearsons correlation: {corr}")
 
 # calculate spearman's correlation
-# Spearman's correlation coefficient = covariance(rank(X), rank(Y)) / (stdv(rank(X)) * stdv(rank(Y)))
+# Spearman's correlation coefficient =
+# covariance(rank(X), rank(Y)) / (stdv(rank(X)) * stdv(rank(Y)))
 corr, _ = spearmanr(data1, data2)
-print('Spearmans correlation: %.3f' % corr)
+print(f"Spearmans correlation: {corr}")
